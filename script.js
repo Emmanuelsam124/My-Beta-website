5821fabd99c413f2bd51312ccedffb4dee4af95b
Emmanuel Samuel <58230732+Emmanuelsam124@users.noreply.github.com>
2022-03-01 22:09:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
- var theme = document.getElementById('sun-theme');
+        var theme = document.getElementById('sun-theme');
         theme.addEventListener('click', addFunction);
 
 
         function addFunction() {
+
+            const button = document.getElementById("btn");
+            if ((button.style.backgroundColor == "black") && (button.style.color == "white")) {
+                (button.style.backgroundColor = "white") && (button.style.color = "black");
+            } else {
+                (button.style.backgroundColor = "black") && (button.style.color = "white");
+            }
             const body = document.getElementById("body-theme");
             body.classList.toggle("body");
             if (theme.innerHTML === "☀") {
